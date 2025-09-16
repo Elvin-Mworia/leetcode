dd6687b16f6bfda1f89326cdab831cadd34d828a
Elvin-Mworia <elvinmworia@gmail.com>
2025-09-16 07:27:00 +0300

    count odd numbers between an interval

---
+++ largestNum.py
@@ -0,0 +1,16 @@
+from functools import cmp_to_key
+class Solution:
+    def largest(self,nums:list[int])->str:
+        for i,n in enumerate(nums):
+            nums[i]=str(n)
+        def compare(n1,n2):
+            if n1+n2>n2+n1:
+                return -1
+            else:
+                return 1
+
+        nums=sorted(nums,key=cmp_to_key(compare))
+        return str(int("".join(nums)))
+
+problem=Solution()
+print(problem.largest([9,54,34,3,30]))
